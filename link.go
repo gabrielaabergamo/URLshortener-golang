99651a83e625c91eaa2418e03cd72274d95f36c5
gabrielaabergamo <gabiibergamo@gmail.com>
2021-12-08 17:03:18 -0300

    transformando struct em json

--- link.go
+++ link.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"time"
 
 	"github.com/dchest/uniuri"
@@ -8,31 +10,40 @@ import (
 )
 
 //struct com os dados da URL
-type url struct {
-	id          string
-	processedAt string
-	duration    string
-	shortURL    string
-	originalURL string
+type Url struct {
+	ID          string
+	ProcessedAt string
+	Duration    string
+	ShortURL    string
+	OriginalURL string
 }
 
 //struct teste
-var aux = url{
-	shortURL:    "nenhuma",
-	originalURL: "oi",
+var aux = &Url{
+	ShortURL:    "nenhuma",
+	OriginalURL: "oi",
 }
 
 //função que encurta a URL
 func URLCurta(txt string) {
-	aux.id = uuid.NewV4().String()
-	aux.originalURL = txt
-	aux.shortURL = "go.io/" + uniuri.NewLen(6)
+	aux.ID = uuid.NewV4().String()
+	aux.OriginalURL = txt
+	aux.ShortURL = "go.io/" + uniuri.NewLen(6)
 	TimeCalc()
 }
 
 //calcula o dia e horário em que foi processado e a duração
 func TimeCalc() {
 	start := time.Now()
-	aux.processedAt = start.Format("2006-01-02 15:04:05")
-	aux.duration = time.Since(start).String()
+	aux.ProcessedAt = start.Format("2006-01-02 15:04:05")
+	aux.Duration = time.Since(start).String()
+}
+
+//transforma a struct em JSON
+func TransfJson() string {
+	auxjson, err := json.Marshal(aux)
+	if err != nil {
+		fmt.Println("erro", err)
+	}
+	return string(auxjson)
 }
