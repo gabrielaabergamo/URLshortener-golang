23028ea55e83657a7c88ad2ed4e83971b2cf286d
gabrielaabergamo <gabiibergamo@gmail.com>
2021-12-09 12:03:24 -0300

    slice para salvar dados de urls

--- link.go
+++ link.go
@@ -18,34 +18,43 @@ type Url struct {
 	OriginalURL string
 }
 
-var aux = make([]Url, 10)
+var listaURL = make([]Url, 0)
 
 //struct teste
-// var aux = Url{
-// 	ShortURL:    "nenhuma",
-// 	OriginalURL: "oi",
-// }
+var aux = Url{
+	ShortURL:    "nenhuma",
+	OriginalURL: "oi",
+}
 
 //função que encurta a URL
 func URLCurta(txt string) {
-	aux[0].ID = uuid.NewV4().String()
-	aux[0].OriginalURL = txt
-	aux[0].ShortURL = "go.io/" + uniuri.NewLen(6)
+	aux.ID = uuid.NewV4().String()
+	aux.OriginalURL = txt
+	aux.ShortURL = "go.io/" + uniuri.NewLen(6)
 	TimeCalc()
+	listaURL = append(listaURL, aux)
 }
 
 //calcula o dia e horário em que foi processado e a duração
 func TimeCalc() {
 	start := time.Now()
-	aux[0].ProcessedAt = start.Format("2006-01-02 15:04:05")
-	aux[0].Duration = time.Since(start).String()
+	aux.ProcessedAt = start.Format("2006-01-02 15:04:05")
+	aux.Duration = time.Since(start).String()
 }
 
 //transforma a struct em JSON
 func TransfJson() string {
-	auxjson, err := json.Marshal(aux[0])
+	auxjson, err := json.Marshal(aux)
 	if err != nil {
 		fmt.Println("erro", err)
 	}
+	//printslice()
 	return string(auxjson)
 }
+
+//teste para checar se os dados estão sendo salvos corretamente
+// func printslice() {
+// 	for _, i := range listaURL {
+// 		log.Println(i)
+// 	}
+// }
