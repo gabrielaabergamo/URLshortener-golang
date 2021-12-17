715fcdaf0950b7380a0f918fb4b2cfcbd9792779
gabrielaabergamo <gabiibergamo@gmail.com>
2021-12-17 15:07:56 -0300

    verificação url

--- server.go
+++ server.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"regexp"
+	"net/url"
 
 	"github.com/gorilla/mux"
 )
@@ -12,22 +12,27 @@ import (
 //https://ichi.pro/pt/sua-primeira-api-rest-em-golang-com-mux-202836347743488
 //https://blog.logrocket.com/making-http-requests-in-go/
 
-func verificarURL(url string) bool {
-	match, _ := regexp.MatchString("[(http(s)?):\\//\\//(www\\.)?a-zA-Z0-9\\+]{2,256}\\.[a-z]{2,6}", url)
-	return match
-}
-
-func verificarCodigo(codigo string) bool {
-	//match, _ := regexp.MatchString("^[a-zA-Z0-9]{6,6}$", codigo)
-	//return match
+func verificarURL(txt string) bool {
+	// match, _ := regexp.MatchString("[(http(s)?):\\//\\//(www\\.)?a-zA-Z0-9\\+]{2,256}\\.[a-z]{2,6}", url)
+	// return match
+	_, err := url.ParseRequestURI(txt)
+	if err != nil {
+		return false
+	}
 	return true
 }
+
+// func verificarCodigo(codigo string) bool {
+// 	//match, _ := regexp.MatchString("^[a-zA-Z0-9]{6,6}$", codigo)
+// 	//return match
+// 	return true
+// }
 
 func MetodoGet(router *mux.Router) {
 	router.HandleFunc("/retrieve/", func(res http.ResponseWriter, req *http.Request) {
 		vars := mux.Vars(req)
 		res.Header().Set("Content-Type", "application/json")
-		verificacao := verificarCodigo(vars["nameog"])
+		verificacao := verificarURL(vars["nameog"])
 		if verificacao {
 			fmt.Fprint(res, URLGet(vars["nameog"]))
 		} else {
@@ -41,18 +46,13 @@ func MetodoPost(router *mux.Router) {
 	router.HandleFunc("/send/", func(res http.ResponseWriter, req *http.Request) {
 		vars := mux.Vars(req)
 		res.Header().Set("Content-Type", "application/json")
-		//fmt.Fprint(res, vars, vars["name"])
-		//res.WriteHeader(http.StatusOK)
 		longURL := vars["name"]
 		verificacao := verificarURL(longURL)
-		// valid := validation.Validate(longURL, validation.Required, validation.Length(5, 100), is.URL)
-		// log.Println(valid, longURL)
 		if verificacao {
 			fmt.Fprint(res, URLPost(longURL))
 		} else {
 			fmt.Fprint(res, "URL inválida")
 		}
-		//URLPost(longURL)
 	}).Methods("POST").Queries("url", "{name}")
 }
 
