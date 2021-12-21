fadd071ed3d68d617cb279bebdb22af58c74cb18
gabrielaabergamo <gabiibergamo@gmail.com>
2021-12-21 15:52:40 -0300

    usando assertion nos testes unitários

--- go.mod
+++ go.mod
@@ -10,7 +10,12 @@ require github.com/satori/go.uuid v1.2.0
 
 require (
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.1.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- link_test.go
+++ link_test.go
@@ -3,34 +3,47 @@ package main
 import (
 	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestUUID(t *testing.T) {
 	aux := GeradorUUID()
+	aux_type := reflect.TypeOf(aux).String()
 
-	if reflect.TypeOf(aux).String() != "uuid.UUID" {
-		t.Errorf("GeradorUUID() failed, expected type uuid.UUID, got %v", reflect.TypeOf(aux).String())
-	} else {
-		t.Logf("GeradorUUID() success, expected type uuid.UUID, got %v", reflect.TypeOf(aux).String())
-	}
+	// if aux_type != "uuid.UUID" {
+	// 	t.Errorf("GeradorUUID() failed, expected type uuid.UUID, got %v", aux_type)
+	// } else {
+	// 	t.Logf("GeradorUUID() success, expected type uuid.UUID, got %v", aux_type)
+	// }
+	assert := assert.New(t)
+	assert.Equal(aux_type, "uuid.UUID", "Expected type uuid.UUID, got type uuid.UUID")
 }
 
 func TestCodigo(t *testing.T) {
 	aux := GeradorCodigo()
 
-	if len(aux) != 6 {
-		t.Errorf("GeradorCodigo() failed, expected length 6, got %v instead", len(aux))
-	} else {
-		t.Logf("GeradorCodigo() success, expected length 6, got %v", len(aux))
-	}
+	// if len(aux) != 6 {
+	// 	t.Errorf("GeradorCodigo() failed, expected length 6, got %v instead", len(aux))
+	// } else {
+	// 	t.Logf("GeradorCodigo() success, expected length 6, got %v", len(aux))
+	// }
+	assert := assert.New(t)
+	assert.Equal(len(aux), 6, "Expected length 6, got 6")
 }
 
 func TestUrl(t *testing.T) {
 	aux := GeradorURL("abs67D")
 
-	if aux != "http://go.io/abs67D" {
-		t.Errorf("GeradorURL(\"abs67D\") failed, expected %v, got %v", "http://go.io/abs67D", aux)
-	} else {
-		t.Logf("GeradorURL(\"abs67D\") success, expected %v, got %v", "http://go.io/abs67D", aux)
-	}
+	// if aux != "http://go.io/abs67D" {
+	// 	t.Errorf("GeradorURL(\"abs67D\") failed, expected %v, got %v", "http://go.io/abs67D", aux)
+	// } else {
+	// 	t.Logf("GeradorURL(\"abs67D\") success, expected %v, got %v", "http://go.io/abs67D", aux)
+	// }
+	assert := assert.New(t)
+	assert.Equal(aux, "http://go.io/abs67D", "Expected 'http://go.io/abs67D', got 'http://go.io/abs67D'")
+}
+
+func TestTeste(t *testing.T) {
+	assert.True(t, true, "True is true!")
 }
